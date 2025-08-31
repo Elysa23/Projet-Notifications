d7333ed847d4946dc6c3ea0afd6ca3d3e6dfbc65
Elysa <elysanambinintsoa3@gmail.com>
2025-08-31 20:44:50 +0300

    Bouton d'envoi couleur MAJ

--- app/page.tsx
+++ app/page.tsx
@@ -123,7 +123,7 @@ export default function Home() {
               </button>
               <button
                 onClick={envoiMessagePerso}
-                className="px-4 py-2 bg-blue-500 text-white rounded"
+                className="px-4 py-2 bg-sky-600 text-white rounded"
               >
                 Envoyer
               </button>
